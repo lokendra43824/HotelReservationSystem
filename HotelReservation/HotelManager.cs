88eb0be69172e4134f5d71ddce1cc8282bb49bc1
Lokendra <lokilokendra12345@gmail.com>
2020-11-07 09:20:32 +0530

    [lokendra] added a code to uc_1_ability_to_add_hotel

---
+++ HotelReservation/HotelManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation
+{
+    public class HotelManager
+    {
+        public List<Hotel> hotelList = new List<Hotel>();
+
+        public void AddHotel(Hotel newHotel)
+        {
+            hotelList.Add(newHotel);
+        }
+
+        public void DisplayHotels()
+        {
+            foreach (var hotel in hotelList)
+            {
+                Console.WriteLine(hotel.hotelName + "\t" + hotel.rateOfRegularCustomer);
+            }
+        }
+    }
+}
--- HotelReservation/Program.cs
+++ HotelReservation/Program.cs
@@ -6,7 +6,38 @@ namespace HotelReservation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Hotel Reservation System\n\n");
+            HotelManager manager = new HotelManager();
+            bool val = true;
+            while (val)
+            {
+                Console.WriteLine("Choose among the following option\n1.Add Hotel\t2.Display Hotel\t 3.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            manager.AddHotel(new Hotel("Bridgewood", 160));
+                            manager.AddHotel(new Hotel("Ridgewood", 220));
+                            manager.AddHotel(new Hotel("Lakewood", 110));
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            manager.DisplayHotels();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            val = false;
+                            break;
+                        }
+
+                    default: break;
+                }
+            }
         }
     }
 }
