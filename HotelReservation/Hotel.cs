a8e4466b9eb29d440600db076156ed7aa83d3b13
Lokendra <lokilokendra12345@gmail.com>
2020-11-28 15:47:31 +0530

    [lokendra] refactored a code

--- HotelReservation/Hotel.cs
+++ HotelReservation/Hotel.cs
@@ -6,39 +6,19 @@ namespace HotelReservation
 {
     public class Hotel
     {
-        internal string rateOfRegularCustomer;
-        private string v1;
-        private int v2;
-
         public string hotelName { get; set; }
-        public int weekdayRate { get; set; }
-        public int weekendRate { get; set; }
-        public int weekendLoyaltyRate { get; set; }
-        public int weekdayLoyaltyRate { get; set; }
-        public int rating { get; set; }
-       public Hotel()
+        public int rateOfRegularCustomer { get; set; }
+
+        public Hotel()
         {
             hotelName = "";
-            weekendRate = 0;
-            weekdayRate = 0;
-            weekdayLoyaltyRate = 0;
-            weekendLoyaltyRate = 0;
-            rating = 0;
-        }
-        public Hotel(string name, int mfrate, int ssrate, int weekdayL, int weekendL, int rating)
-        {
-            this.hotelName = name;
-            this.weekendRate = ssrate;
-            this.weekdayRate = mfrate;
-            this.rating = rating;
-            this.weekendLoyaltyRate = weekendL;
-            this.weekdayLoyaltyRate = weekdayL;
+            rateOfRegularCustomer = 0;
         }
 
-        public Hotel(string v1, int v2)
+        public Hotel(string name, int rate)
         {
-            this.v1 = v1;
-            this.v2 = v2;
+            this.hotelName = name;
+            this.rateOfRegularCustomer = rate;
         }
     }
 }
